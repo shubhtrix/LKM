c0dc8190c13e4604f90ab4ee73544207d05dd3a2
root <shubham.sharma1@inventum.net>
2019-03-23 00:01:42 +0530

    Copied code for vram (video RAM) compiled and tested

--- 05_GNRIC_HW_ACCESS/gnric_hw_accss.c
+++ 05_GNRIC_HW_ACCESS/gnric_hw_accss.c
@@ -1,3 +1,4 @@
+/* Accessing the video RAM of DOS days. */
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -11,66 +12,97 @@
  * copy_to_user and copy_from_user          */
 #include <linux/uaccess.h>
 
+/* The corresponding APIs (prototyped in <asm/io.h>) for 
+mapping and unmapping the device bus addresses to virtual addresses
+*/
+#include <asm/io.h>
+
+#define VRAM_BASE 0x000A0000
+#define VRAM_SIZE 0x00020000
+
 /* Need to have some kind of a user buffer. */
-char usr_bfr[512];
-static int count=0;
+static void __iomem *vram;
 
 static dev_t first;		    //One vaiable containing Majaor & Minor Nos.
 static struct cdev c_dev;	//Global variable for character device structure 
 static struct class *cl;	//Global variable for device class 
 
-static int my_null_open(struct inode *i, struct file *f) {
+static int my_open(struct inode *i, struct file *f) {
 	printk(KERN_INFO "MY_NULL DEVICE_OPEN()\n");
 	return 0;
 }
-static int my_null_close(struct inode *i, struct file *f) {
+static int my_close(struct inode *i, struct file *f) {
 	printk(KERN_INFO "MY_NULL DEVICE_CLOSE()\n");
 	return 0;
 }
-static ssize_t my_null_read(struct file *f, char __user *buf,
+static ssize_t my_read(struct file *f, char __user *buf,
 			size_t len, loff_t *off) {
-	printk(KERN_INFO "MY_NULL DEVICE_READ()\n");
-    // Need to write len bytes to buffer buf
-    // buf[0] = usr_bfr[0];        // Good Practice to use copy_to_user instead
-    if (copy_to_user (buf, usr_bfr, 1) != 0) {
-        return -EFAULT;
-    } else {
-        printk(KERN_INFO "READ Data from user buffer:: %c\n", buf[0]);
-        if (count) {
-            return 0;
-        } else {
-            ++count;
-            return 1;
-        }
-        ++off;
+
+    int i;
+    u8 byte;
+    printk(KERN_INFO "MY_NULL DEVICE_READ()\n");
+
+    if (*off >= VRAM_SIZE) {
+        return 0;
     }
-}
-static ssize_t my_null_write(struct file *f, const char __user *buf,
-			size_t len, loff_t *off) {
-	printk(KERN_INFO "MY_NULL DEVICE_WRITE()\n");
-    //usr_bfr[0] = buf[0];        // Good Practice to use copy_from_user instead
+    if (*off + len > VRAM_SIZE) {
+        len = VRAM_SIZE - *off;
+    }
+
+    for (i=0;i<len;i++) {
     
-    if (copy_from_user (usr_bfr,buf,1) !=0) {
-        return -EFAULT;
-    } else {
-        printk(KERN_INFO "Buffer data provided by user :: %c", usr_bfr[0]);
-	    printk(KERN_INFO "MY_NULL DEVICE_WRITE LENGTH: %ld",len);
-        return len;
+        byte = ioread8((u8 *)vram + *off + i);
+        if (copy_to_user (buf + i, &byte, 1)) {
+            return -EFAULT;
+        }
     }
+    *off += len;
+
+    return len;
+}
+
+static ssize_t my_write(struct file *f, const char __user *buf,
+			size_t len, loff_t *off) {
+    int i;
+    u8 byte;
+    printk(KERN_INFO "MY_NULL DEVICE_WRITE()\n");
+
+    if (*off >= VRAM_SIZE) {
+        return 0;
+    }
+    if (*off + len > VRAM_SIZE) {
+        len = VRAM_SIZE - *off;
+    }
+
+    for (i=0;i<len;i++) {
+    
+        if (copy_from_user (&byte, buf + i, 1)) {
+            return -EFAULT;
+        }
+        iowrite8(byte, (u8 *)vram + *off + i);
+    }
+    *off += len;
+
+    return len;
 }
 
 // FILE OPERATIONS SRUCTURE FROM "linux/fs.h"
 static struct file_operations my_ops = {
 	.owner = THIS_MODULE,
-	.open = my_null_open,
-	.release = my_null_close,
-	.read = my_null_read,
-	.write = my_null_write,
+	.open = my_open,
+	.release = my_close,
+	.read = my_read,
+	.write = my_write,
 };
 
 static int __init IN(void) {
-	
-    if(alloc_chrdev_region( &first, 0, 2, "SHUBH") < 0)
+
+    if ((vram = ioremap(VRAM_BASE, VRAM_SIZE)) == NULL)
+    {
+        printk(KERN_ERR "Mapping video RAM failed\n");
+        return -1;
+    }    
+    if(alloc_chrdev_region( &first, 0, 2, "vram") < 0)
 		return -1;
 
 	printk(KERN_INFO "Major no.: %d\tMinor no.: %d\n", MAJOR(first), MINOR(first));
@@ -81,14 +113,13 @@ static int __init IN(void) {
 		return -1;
 	}
 	
-	if (device_create(cl, NULL, first, NULL, "gnric_hw_accss") == NULL) {
+	if (device_create(cl, NULL, first, NULL, "vram") == NULL) {
 		class_destroy(cl);
 		unregister_chrdev_region(first, 2);
 		return -1;
 	}
 
 	cdev_init(&c_dev, &my_ops);
-	
 	if (cdev_add(&c_dev, first, 1) == -1) {
 		device_destroy(cl, first);
 		class_destroy(cl);
@@ -105,6 +136,7 @@ static void __exit OUT(void) {
 	device_destroy(cl, first);
 	class_destroy(cl);
 	unregister_chrdev_region(first, 2);
+    iounmap(vram);
 	printk(KERN_INFO "Exiting FROM TEST MODULE!!!");
 }
 
